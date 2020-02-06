ecf63552efca8829159e125aaf1db4952b06541f
bashow <bashow0316@gmail.com>
2020-02-06 17:56:27 +0900

    Add docker-compose.yml & main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+# Make by bashow
+# bashow0316@gmail.com
+
+# monitoring ping system
+
+
+import os
+import sys
+import pings
+import socket
+import time
+
+
+def main():
+    print('Hello Monitor ping')
+
+
+if __name__ == '__main__':
+
+    main()
+
